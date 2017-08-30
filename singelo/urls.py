fad5f1ae6860b800814f3c944230a91e1570ec2a
Luis Alves <luisalves05@gmail.com>
2017-08-30 14:05:06 -0300

    Creating post template and views

--- singelo/urls.py
+++ singelo/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from singelo.views import Index
+from singelo.views import IndexView, PostView
 
 urlpatterns = [
-    url(r'^$', Index.as_view()),
+    url(r'^$', IndexView.as_view()),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1})/(?P<slug>[\w-]+)/$', PostView.as_view()),
 ]
--- singelo/views.py
+++ singelo/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.core.urlresolvers import resolve
+
 from django.views import View
 from .models import Post
 
-# Create your views here.
-class Index(View):
+
+class IndexView(View):
     def get(self, request):
         posts = Post.objects.all()
-        context = {"posts": posts}
+        context = {"posts": posts, "app_name": resolve(request.path).app_name}
         return render(request, 'singelo/index.html', context)
+
+class PostView(View):
+    def get(self, request, year, month, slug):
+        post = Post.objects.get(post_slug = slug)
+        context = {"post": post}
+        return render(request, 'singelo/post.html', context)
